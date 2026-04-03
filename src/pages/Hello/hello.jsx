8c20fad5ad279bdd6a5817390dbc35ec6e59bd7d
MinhTri94-dmi <dangminhtri904@gmail.com>
2026-04-03 15:05:53 +0700

    te

--- src/pages/Hello/hello.jsx
+++ src/pages/Hello/hello.jsx
@@ -2,6 +2,8 @@ const Hello = () => {
   return (
     <div>
       <h1>Hello, World!Xin Chao moi nguoi</h1>
+      <h1>Hello, World!Xin Chao moi nguoi</h1>
+      <h1>Hello, World!Xin Chao moi nguoi</h1>
     </div>
   )
 }

957c7b0e0e31711be720f23b02f451a0af1ef0e5
DMiTri904 <169603869+DMiTri904@users.noreply.github.com>
2026-04-12 15:44:52 +0700

    Code table 2

--- src/pages/Hello/hello.jsx
+++ src/pages/Hello/hello.jsx
@@ -19,6 +19,16 @@ const Hello = () => {
           <td>30</td>
         </tr>
       </table>
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+        </tr>
+        <tr>
+          <td>John</td>
+          <td>30</td>
+        </tr>
+      </table>
     </div>
   )
 }
